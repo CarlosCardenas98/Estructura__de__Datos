f5be925701643c61e19d5b6a0692ccd300df7cc6
CarlosCardenas98 <167553997+CarlosCardenas98@users.noreply.github.com>
2024-05-03 17:19:11 -0500

    Laboratorio_01

--- Curso_Estructura_De_Datos/Laboratorio_01/Program.cs
+++ Curso_Estructura_De_Datos/Laboratorio_01/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hola Mundo");
+﻿// Programa que sume los n primeros numeros
+
+var n = 50;
+var suma = 0;
+for (var i = 1; i <=n; i++)
+
+    suma = suma + i;
+
+Console.WriteLine($"la suma de los {n} primeros numeros es: {suma} ");
